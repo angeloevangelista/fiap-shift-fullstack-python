d2fd1ff4265b166b9519a9e21fc6c5f872f14516
Angelo Evangelista <angeloevan.ane@gmail.com>
2025-04-24 22:44:28 -0300

    feat: add web app

---
+++ bookstore/bookstore-web/src/routes/Home.jsx
@@ -0,0 +1,52 @@
+import { useState, useEffect } from 'react';
+import axios from 'axios';
+import '../css/home.css'
+
+
+
+const Home = () => {
+  const [livros, setLivros] = useState([]);
+
+  const fetchLivros = async () => {
+    try {
+      // Endpoint sendo chamado pela função axios usando a requisição GET
+      const response = await axios.get('http://localhost:3000/livros', {
+        headers: {
+          Authorization: `Bearer ${localStorage.getItem('token')}`, // Verifica a autenticação
+        },
+      });
+      setLivros(response.data);
+    } catch (error) {
+      console.error('Erro ao buscar livros', error);
+    }
+  };
+
+  useEffect(() => {
+    fetchLivros();
+  }, []);
+
+  return (
+    <section className="home"> 
+      <section className="cardsProd">
+        <h2 className="title">Dashboard Livraria</h2>
+
+        <div className="card-container">
+          <h3>Livros</h3>
+          <div className="cards">
+            {livros.map((livro, index) => (
+              <div key={index} className="card">
+                <h4>{livro.titulo}</h4>
+                <p>Autor(es): {livro.autor_nome || "N/A"}</p>
+                <p>Categoria(s): {livro.categoria_nome || "N/A"}</p>
+                <p>Editora(s): {livro.editora_nome || "N/A"}</p>
+                <p>Quantidade restante: {livro.quantidade}</p> {/* Adicionado aqui */}
+              </div>
+            ))}
+          </div>
+        </div>
+      </section>
+    </section>
+  );
+};
+
+export default Home;
